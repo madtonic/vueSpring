9afab677340af53c0b64ed1994aca2e7e7604649
madtonic <kdh8400@naver.com>
2022-01-09 14:57:03 +0900

    first

---
+++ src/main/java/com/taskalie/TaskAgileApplication.java
@@ -0,0 +1,13 @@
+package com.taskalie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskAgileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskAgileApplication.class, args);
+	}
+
+}
